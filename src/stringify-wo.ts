0a8427b13f3b39a3803bd924263b9fec75f0a9b5
Charley Collins <charley@collins.ch>
2026-04-06 21:06:58 +0200

    Open current Week File

--- src/stringify-wo.ts
+++
@@ -1,8 +0,0 @@
-import { WoElement, woFileStructure, WoFileStructure } from './types';
-
-export function stringifyWo(elements: Record<WoFileStructure, WoElement[]>) {
-  const lines = woFileStructure.flatMap((key) => [
-    ...elements[key].map((element) => element.content.join('\n')),
-  ]);
-  return lines.join('\n');
-}
--- src/util.ts
+++
@@ -1,5 +0,0 @@
-import { createHash } from 'crypto';
-
-export const createLocalId = (input: string): string => {
-  return createHash('sha1').update(input).digest('hex');
-};
